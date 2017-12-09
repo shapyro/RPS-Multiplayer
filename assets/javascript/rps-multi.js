65951dbc188674111cc78cad1c081757c8b7adc2
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2017-12-08 23:31:41 -0600

    wireframe

---
+++ assets/javascript/rps-multi.js
@@ -0,0 +1,10 @@
+//  enter username
+//    write to Firebase
+//    write to DOM
+//  pick Rock Paper OR Scissors
+//    write to Firebase
+//    write to DOM
+//    Which Player won?
+//      display winner
+//  Display Who won
+
