0c106f144cc562d8ffdc69a7aaeb3fca63ba60e9
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2017-12-09 23:13:04 -0600

    i am you and you are me

--- assets/javascript/rps-multi.js
+++ assets/javascript/rps-multi.js
@@ -28,6 +28,35 @@ firebase.initializeApp(config);
 
 //  establish db
 var database = firebase.database();
+var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is a special location provided by Firebase that is updated every time
+// the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+
+  // If they are connected..
+  if (snap.val()) {
+
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snap) {
+  // console.log(snap.numChildren());
+
+  // Display the viewer count in the html.
+  // The number of online users is the number of children in the connections list.
+  $("#watchers").text(snap.numChildren());
+});
 
 var wins = 0;
 var losses = 0;
@@ -55,13 +84,23 @@ $(document).ready(function(){
     //  set playerInfo
     //  Array maybe in firebase?
     database.ref().push(playerInfo);  
+    // playerList.push(playerName);
     $('#playerName').val('');
+    var connectedRef = database.ref(".info/connected");
+    // console.log(connectedRef.key);
+
+    $.extend(playerInfo,{status: connectedRef.key});
+    // console.log(playerInfo);
+    database.ref().push(playerInfo);  
+    
 
   database.ref().on("child_added", function(childSnapshot, prevChildKey){
-    console.log(childSnapshot.key);
-    console.log(childSnapshot.val());
+    // console.log(childSnapshot.key);
+    // console.log(childSnapshot.val().name);
     // console.log(childSnapshot.key());
     var playerName = childSnapshot.val().name;// .name;
+    var playerID = childSnapshot.key;
+
 
     $('.playerInfo').empty();
     $('.playerInfo').append(`<h1>${playerName}</h1>`);
