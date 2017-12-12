af44baf71706fdb2e7ab46eea0945c3a1bdb7141
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2017-12-11 18:30:45 -0600

    working on firebase

--- assets/javascript/rps-multi.js
+++ assets/javascript/rps-multi.js
@@ -13,101 +13,87 @@
 //    Show which choice trumped the other
 //  --------------------------------------
 //  is there a timer?
+//    click draw for who wins most clicks in 10 seconds?
 //  is there a chat app
-
-//  dbconfig
-var config = {
-  apiKey: "AIzaSyDnH8rHXPVRXREfAZd6fltrfQDvliOScV8",
-  authDomain: "rps-multiplayer-c3f4a.firebaseapp.com",
-  databaseURL: "https://rps-multiplayer-c3f4a.firebaseio.com",
-  projectId: "rps-multiplayer-c3f4a",
-  storageBucket: "rps-multiplayer-c3f4a.appspot.com",
-  messagingSenderId: "334508367375"
-};
-firebase.initializeApp(config);
-
-//  establish db
-var database = firebase.database();
-var connectionsRef = database.ref("/connections");
-
-// '.info/connected' is a special location provided by Firebase that is updated every time
-// the client's connection state changes.
-// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-var connectedRef = database.ref(".info/connected");
-
-// When the client's connection state changes...
-connectedRef.on("value", function(snap) {
-
-  // If they are connected..
-  if (snap.val()) {
-
-    // Add user to the connections list.
-    var con = connectionsRef.push(true);
-
-    // Remove user from the connection list when they disconnect.
-    con.onDisconnect().remove();
-  }
-});
-
-// When first loaded or when the connections list changes...
-connectionsRef.on("value", function(snap) {
-  // console.log(snap.numChildren());
-
-  // Display the viewer count in the html.
-  // The number of online users is the number of children in the connections list.
-  $("#watchers").text(snap.numChildren());
-});
-
-var wins = 0;
-var losses = 0;
-var num_players = 2;
-//  var playerName;
-var playerList = [];
-
-//  get DOM ready for clicks
+//  
 $(document).ready(function(){
+
+  //  dbconfig
+  var config = {
+    apiKey: "AIzaSyDnH8rHXPVRXREfAZd6fltrfQDvliOScV8",
+    authDomain: "rps-multiplayer-c3f4a.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-c3f4a.firebaseio.com",
+    projectId: "rps-multiplayer-c3f4a",
+    storageBucket: "rps-multiplayer-c3f4a.appspot.com",
+    messagingSenderId: "334508367375"
+  };
+  firebase.initializeApp(config);
+
+  //  establish db
+  var database = firebase.database();
+  // var chats = database.ref("chat");
+  var connections = database.ref("/connected");
+
+  // '.info/connected' is a special location provided by Firebase that is updated every time
+  // the client's connection state changes.
+  // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+  // var connectedRef = database.ref(".info/connected");
+
+  var player = {
+    number: '0',
+    name: '',
+    wins: '0',
+    losses: '0',
+    choice: ''
+  }
+
+  var opponent = {
+    number: '0',
+    name: '',
+    wins: '0',
+    losses: '0',
+    choice: ''
+  }
 
   $('#playerName').click(function() {
     $('#playerName').val('');
   });
 
-  //  Enter Player Name
   $('#submit').click(function(event){
-    event.preventDefault();
-    if (playerList.length <= 2){
 
-    var playerName = $('#playerName').val().trim();
-    var playerInfo = {
-      name: playerName,
-    };
-    
-    //  set playerInfo
-    //  Array maybe in firebase?
-    database.ref().push(playerInfo);  
-    // playerList.push(playerName);
-    $('#playerName').val('');
-    var connectedRef = database.ref(".info/connected");
-    // console.log(connectedRef.key);
-
-    $.extend(playerInfo,{status: connectedRef.key});
-    // console.log(playerInfo);
-    database.ref().push(playerInfo);  
-    
-
-  database.ref().on("child_added", function(childSnapshot, prevChildKey){
-    // console.log(childSnapshot.key);
-    // console.log(childSnapshot.val().name);
-    // console.log(childSnapshot.key());
-    var playerName = childSnapshot.val().name;// .name;
-    var playerID = childSnapshot.key;
+      //  take player name from input
+      player.name = $('#playerName').val().trim();
+      player.number = number
+      //  set player info in datbase
+      database.ref("/gameroom").push(player);  
+      $('.playerInfo').empty();
+      $('.playerInfo').append(`<div id='player'>${player.name}</div>`)
+  
+  
+    // opponent.name = $('#playerName').val().trim();
+    // //  set opponent info in datbase
+    // database.ref().push(opponent);  
+    // $('.playerInfo').empty();
+    // $('.playerInfo').append(`<div id='opponent'>${opponent.name}</div>`);
 
 
-    $('.playerInfo').empty();
-    $('.playerInfo').append(`<h1>${playerName}</h1>`);
+    database.ref("/gameroom").on("child_added", function(childSnapshot){
+      player.name = childSnapshot.val().name;
+      console.log(childSnapshot.key);
+      // console.log(childSnapshot.key().name);
     }, function(errorObject) {
-     console.log("The read failed: " + errorObject.code);
+    console.log("The read failed: " + errorObject.code);
+    });
+
   });
-  };
-});
+
+  $('#resetDB').on('click', function(){
+    clearDB();
+  })
+
+  function clearDB(){
+    database.remove(childSnapshot);
+    // database.childSnapshot.key.remove();
+  }
 
 });
